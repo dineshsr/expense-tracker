0f39193390d3e01e724accb4ff85974ec1fd8528
dineshsr <dineshsr@zohomail.com>
2020-12-24 09:42:01 +0530

    [UPDATE]: Updated styles along with mobile view.

--- expense-tracker-client/src/components/auth/LandingPage.js
+++ expense-tracker-client/src/components/auth/LandingPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Carousel, Col, Container, Content, Panel, Row } from "rsuite";
+import { Carousel } from "rsuite";
 
 import "rsuite/dist/styles/rsuite-default.css";
 import logo from "../../images/logo-icon.png";
@@ -11,58 +11,48 @@ import SignIn from "./SignIn";
 const LandingPage = () => {
 	return (
 		<div className="show-container">
-			<Container>
-				<Content>
-					<span className="ellipseBg"></span>
-					<Panel className="totalPanel verticalCenter">
-						<Row>
-							<Col xsHidden md={12}>
-								<Panel
-									className="panelLeft"
-									header={<h3>Expense Manager</h3>}
-								>
-									<Carousel
-										autoplay
-										className="custom-slider"
-									>
-										<img
-											src="https://via.placeholder.com/600x250/8f8e94/FFFFFF?text=1"
-											height="250"
-											alt="images"
-										/>
-										<img
-											src="https://via.placeholder.com/600x250/8f8e94/FFFFFF?text=2"
-											height="250"
-											alt="images"
-										/>
-										<img
-											src="https://via.placeholder.com/600x250/8f8e94/FFFFFF?text=3"
-											height="250"
-											alt="images"
-										/>
-										<img
-											src="https://via.placeholder.com/600x250/8f8e94/FFFFFF?text=4"
-											height="250"
-											alt="images"
-										/>
-										<img
-											src="https://via.placeholder.com/600x250/8f8e94/FFFFFF?text=5"
-											height="250"
-											alt="images"
-										/>
-									</Carousel>
-								</Panel>
-							</Col>
-							<Col md={12} className="panelRight">
-								<Panel>
-									<img src={logo} height={75} alt="logo" />
-								</Panel>
-								<SignUp />
-							</Col>
-						</Row>
-					</Panel>
-				</Content>
-			</Container>
+			<span className="ellipseBg"></span>
+			<div className="totalPanel verticalCenter">
+				<div xsHidden md={12} className="panelLeft">
+					<h3>Expense Manager</h3>
+					<br />
+					<Carousel
+						autoplay
+						className="custom-slider"
+						style={{ display: "flex" }}
+					>
+						<img
+							src="https://via.placeholder.com/600x250/8f8e94/FFFFFF?text=1"
+							height="250"
+							alt="images"
+						/>
+						<img
+							src="https://via.placeholder.com/600x250/8f8e94/FFFFFF?text=2"
+							height="250"
+							alt="images"
+						/>
+						<img
+							src="https://via.placeholder.com/600x250/8f8e94/FFFFFF?text=3"
+							height="250"
+							alt="images"
+						/>
+						<img
+							src="https://via.placeholder.com/600x250/8f8e94/FFFFFF?text=4"
+							height="250"
+							alt="images"
+						/>
+						<img
+							src="https://via.placeholder.com/600x250/8f8e94/FFFFFF?text=5"
+							height="250"
+							alt="images"
+						/>
+					</Carousel>
+				</div>
+				<div md={12} className="panelRight">
+					<img src={logo} height={75} alt="logo" />
+					<SignUp />
+				</div>
+			</div>
 		</div>
 	);
 };
