5362b5dcdce8cdad05ef80e131b8e5a2a0e55eae
dineshsr <dineshsr@zohomail.com>
2020-12-28 21:08:02 +0530

    [UPDATE]: Rewriting the code with new UI design.

--- expense-tracker-client/src/components/auth/SignUp.js
+++ expense-tracker-client/src/components/auth/SignUp.js
@@ -1,59 +1,65 @@
 import React from "react";
-import { Button, Divider, Form, FormControl, FormGroup, Panel } from "rsuite";
+import { Button, Panel, Divider, Form, FormControl, FormGroup } from "rsuite";
 
 import "rsuite/dist/styles/rsuite-default.css";
 import "../../styles/general.css";
 
-const SignUp = () => {
+const SignIn = () => {
 	return (
-		<React.Fragment>
-			<Panel header={<h3>Create your account</h3>}>
-				<Form fluid>
-					<FormGroup>
-						<FormControl name="fname" placeholder="First Name" />
-					</FormGroup>
-					<FormGroup>
-						<FormControl name="lname" placeholder="Last Name" />
-					</FormGroup>
-					<FormGroup>
-						<FormControl
-							name="email"
-							style={{ width: "maximum" }}
-							placeholder="Email Address"
-						/>
-					</FormGroup>
-					<FormGroup fluid>
-						<FormControl
-							name="password"
-							type="password"
-							placeholder="Password"
-						/>
-					</FormGroup>
-					<FormGroup>
-						<Button className="primaryButton" block>
-							SIGN UP
-						</Button>
-					</FormGroup>
-					<Divider />
-					<FormGroup>
-						<p style={{ marginBottom: 10 }}>
-							Already have an account?
-							<Button
-								appearance="link"
-								className="linkButton"
-								style={{ marginTop: -2 }}
-							>
-								Sign in
+		<div className="backGround">
+			<div className="Login verticalCenter">
+				<Button className="secButton" style={{ float: "right" }}>
+					Already have an account?
+				</Button>
+				<React.Fragment>
+					<Panel
+						header={
+							<div>
+								<h3>Create your Expensify Account</h3>
+							</div>
+						}
+						style={{ marginTop: "20px" }}
+					/>
+					<Form fluid>
+						<FormGroup>
+							<FormControl
+								name="fname"
+								placeholder="First Name"
+							/>
+						</FormGroup>
+						<FormGroup>
+							<FormControl name="lname" placeholder="Last Name" />
+						</FormGroup>
+						<FormGroup>
+							<FormControl
+								name="email"
+								style={{ width: "maximum" }}
+								placeholder="Email Address"
+							/>
+						</FormGroup>
+						<FormGroup fluid>
+							<FormControl
+								name="password"
+								type="password"
+								placeholder="Password"
+							/>
+						</FormGroup>
+						<FormGroup>
+							<Button className="primaryButton" block>
+								SIGN UP
 							</Button>
-						</p>
-						<Button className="primaryButton" block>
-							Try as Demo User
-						</Button>
-					</FormGroup>
-				</Form>
-			</Panel>
-		</React.Fragment>
+						</FormGroup>
+						<Divider />
+						<FormGroup>
+							<Button className="primaryButton" block>
+								Try as Demo User
+							</Button>
+						</FormGroup>
+					</Form>
+				</React.Fragment>
+			</div>
+		</div>
 	);
 };
 
-export default SignUp;
+export default SignIn;
