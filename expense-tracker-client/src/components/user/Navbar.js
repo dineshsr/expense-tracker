6dfee5bedb08e2d4092aa6aa965616f9539053df
dineshsr <dineshsr@zohomail.com>
2021-01-02 17:24:42 +0530

    [UPDATE]: Added right view for home page view.

--- expense-tracker-client/src/components/user/Navbar.js
+++ expense-tracker-client/src/components/user/Navbar.js
@@ -5,6 +5,7 @@ import dashboard from "../../images/icons/dashboard.png";
 import settings from "../../images/icons/settings.png";
 import wallet from "../../images/icons/wallet.png";
 import transaction from "../../images/icons/transaction.png";
+import UserDashboard from "./UserDashboard";
 
 const Navbar = () => {
 	const [state, setState] = useState({
@@ -24,7 +25,7 @@ const Navbar = () => {
 					onSelect={handleSelect}
 				>
 					<Sidenav.Body>
-						<Nav id="navBar">
+						<Nav id="navBar" className="navShadow">
 							<Nav.Item
 								eventKey="0"
 								icon={
@@ -65,8 +66,37 @@ const Navbar = () => {
 					</Sidenav.Body>
 				</Sidenav>
 			</div>
-			<div id="rightView" className="rightView">
-				From Navbar
+			<div className="rightView">
+				<div className="rightBody">
+					Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+					Est dolore, rem corrupti velit magni tempore natus, quisquam
+					dolorem voluptas numquam quaerat et! Animi sapiente sit
+					perspiciatis voluptatibus ea, nesciunt asperiores! Lorem
+					ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+					facilis quia et nemo laudantium iure autem quae sapiente
+					tenetur harum. Sed minima esse maxime ut dolor eius unde
+					architecto aliquid. Lorem ipsum, dolor sit amet consectetur
+					adipisicing elit. Earum voluptates praesentium ducimus fugit
+					quis libero reprehenderit maxime quisquam ratione laborum?
+					Aperiam accusamus molestias fugiat doloremque harum magnam
+					voluptas, corrupti dolor? Lorem ipsum dolor sit amet
+					consectetur adipisicing elit. Labore in suscipit distinctio,
+					temporibus voluptatibus eligendi similique autem. Facere,
+					repellat nobis facilis sapiente necessitatibus officiis!
+					Dolore minus quos quas doloremque vero. Lorem, ipsum dolor
+					sit amet consectetur adipisicing elit. Sed explicabo
+					mollitia perspiciatis accusamus rem nostrum numquam adipisci
+					soluta, ex consequuntur, laborum id doloribus! Labore amet
+					iusto nisi fuga, pariatur suscipit! Lorem ipsum dolor sit
+					amet consectetur adipisicing elit. Perferendis maiores
+					consectetur eaque, doloremque repellendus cumque adipisci
+					tempore vero excepturi modi corrupti debitis saepe soluta
+					aspernatur, laudantium libero necessitatibus sit eveniet.
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+					Est ex cum minima excepturi rerum quo, accusamus aliquam
+					deserunt necessitatibus id in? Voluptatum, accusamus
+					nesciunt? Omnis tempore harum eum.
+				</div>
 			</div>
 		</div>
 	);
